d4048449cb1bdd19d5259d7403ba0c498107a235
adeviot <50864744+adeviot@users.noreply.github.com>
2020-09-10 03:02:32 +0700

    Update ift.h

--- src/ift.h
+++ src/ift.h
@@ -1,9 +1,15 @@
-#ifndef _IFT_TOOLS_
-#define _IFT_TOOLS_
+#ifndef _IFT_TOOLS_H_
+#define _IFT_TOOLS_H_
+#include <Arduino.h>
+
+class ift
+{
+private:
+    /* data */
+public:
+    String explode(String data = "", char separator = ' ', int index = 0);
+    String explode(String data = "", String separator = " ", int index = 0);
+};
 
 
-
-
-
-
-#endif  //_IFT_TOOLS_
+#endif // _IFT_TOOLS_H_
